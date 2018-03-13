b05772dc042da387704951e20bf2972a8f74be54
Unknown <ccollins311@gmail.com>
2018-03-12 20:24:55 -0600

    Added Chrome and Angular links, Chrome disclaimer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  googleChromeURL = 'https://www.google.com/chrome/';
+  angularURL = 'https://angular.io/';
 }
